9dc6baac7d95045bcc91dd357f9b7d5d389be3dd
ryanmansoor <34032322+ryanmansoor@users.noreply.github.com>
2019-04-21 11:13:36 +0100

    Django Project Crash Course Blog
    Django Framework Blog

---
+++ posts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Posts
+# from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Posts')
+
+    posts = Posts.objects.all()[0:10]
+
+    context = {
+        'title': 'Latest Posts',
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def details(request, id):
+    post = Posts.objects.get(id=id)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'posts/details.html', context)
